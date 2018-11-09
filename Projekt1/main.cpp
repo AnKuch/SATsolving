a0fd9dbceee25ae8e7a5bf7b048952286042da6e
AnKuch <a87kuchar@gmail.com>
2018-11-09 03:07:17 +0100

    Zusammenfügen

--- Projekt1/main.cpp
+++ Projekt1/main.cpp
@@ -6,13 +6,14 @@
 #include "fileExtraction.h"
 //#include "cnf.h"
 
-struct array oneOnEach (int zeichen, int spalten);
+struct arra oneOnEach (int zeichen, int spalten);
 int zeilenSpalten (std::string);
 char setVariables (std::string);
+struct arra same(int zeilen, std::string var[]);
 
 using namespace std;
 
-struct array{string clause[8];};
+struct arra{string *clause;};
 
 int main(int argc, char *argv[])
 {
@@ -22,7 +23,9 @@ int main(int argc, char *argv[])
     int fileLength;
     int zeilenZahlI;
     int spaltenZahlI;
-    char variable[100][100][3];
+    string variablesFix;
+    string belegung;
+    string linear[200];
 
 
     // Dateinamen einlesen
@@ -37,7 +40,6 @@ int main(int argc, char *argv[])
     }
     else
     {
-        string linear[200];
         int l=1;
         // Anzahl der Zeichen um nötige Zahl der Variablen zu ermitteln
         while (getline(file,line))
@@ -51,74 +53,169 @@ int main(int argc, char *argv[])
         int x= zeile.length();
         linear[1] = linear[1].erase(0,x);
         spaltenZahlI = zeilenSpalten(linear[1]);
+        for (int i= 2; i<= zeilenZahlI+1; i++)
+        {
+            belegung = belegung + ' ' + linear[i];
+        }
         cout << spaltenZahlI << " Spalten" << endl;
         fileLength = zeilenZahlI * spaltenZahlI;
 
-        // Belegungen Charvariablen zuweisen
-        for (int i = 2; i<=zeilenZahlI; i++)
+        // Belegungen in String speichern
+        for (int i = 1; i<=belegung.length(); i++)
         {
-            for (int j = 1; j<= linear[i].length(); j++)
-            {
-                switch (linear[i][j])
+                switch (belegung[i])
                 {
-                    case '?' :
-                        {
-                            variable[i-1][j][0] = 1;
-                            variable[i-1][j][1] = 0;
-                            variable[i-1][j][2] = 0;
-                        }
                     case 'B' :
                         {
-                            variable[i-1][j][0] = 0;
-                            variable[i-1][j][1] = 1;
-                            variable[i-1][j][2] = 0;
+                            variablesFix = variablesFix + ' ' + to_string(i);
+                            break;
                         }
                     case 'W' :
                         {
-                            variable[i-1][j][0] = 0;
-                            variable[i-1][j][1] = 0;
-                            variable[i-1][j][2] = 1;
+                            variablesFix = variablesFix + ' ' + to_string(-i);
+                            break;
+                        }
+                    default :
+                        {
+                            variablesFix = variablesFix;
+                            break;
                         }
                 }
-
-            }
         }
 
     }
-    struct array x;
+    struct arra x;
     x = oneOnEach(fileLength,spaltenZahlI);
     cout << x.clause[0] << endl;
+    struct arra y;
+    y = same(zeilenZahlI,linear);
+    cout << y.clause[0] << endl;
+    fstream f;
+    f.open("formular.cnf", ios::out);
+    f << 'p' << " cnf " << fileLength << ' ' << 200 << endl;
+    for (int i=0; i<=sizeof(x);i++)
+    {
+    f << x.clause[i];
+    }
+    for (int i=0; i<=sizeof(y);i++)
+    {
+    f << y.clause[i];
+    }
+    f.close();
     return 0;
 }
 // 1 = schwarz 0 = weiß
-struct array oneOnEach (int zeichen, int spalten)
+// oneOnEach gibt die CNF aus, die prüft, dass max. 2 einer Farbe nebeneinander sind
+struct arra oneOnEach (int zeichen, int spalten)
 {
-    struct array conj;
+    cout << "zeichen: " << zeichen << endl;
+    struct arra conj;
+    conj.clause = new string[2];
     for (int i=1; i<=zeichen; i++)
     {
-        if (i!=1)
+        // erstes Feld
+        if (i==1)
         {
-            if (i<zeichen)
-            {
-                conj.clause[0] = conj.clause[0] + ' ' + to_string(-i) + ' ' + to_string(-(i+1)) + ' ' + to_string(-(i+2)) + '\n';
-                conj.clause[1] = conj.clause[1] + ' ' + to_string(-i) + ' ' + to_string(-(i+1)) + ' ' + to_string(-(i-1)) + '\n';
-                conj.clause[2] = conj.clause[2] + ' ' + to_string(-i) + ' ' + to_string(-(i+spalten)) + ' ' + to_string(-(i+spalten)) + '\n';
-                conj.clause[3] = conj.clause[3] + ' ' + to_string(-i) + ' ' + to_string(-(i+spalten)) + ' ' + to_string(-(i-spalten)) + '\n';
-            }
-            else
-            {
-                conj.clause[1] = conj.clause[1] + ' ' + to_string(-i) + ' ' + to_string(-(i+1)) + ' ' + to_string(-(i-1)) + '\n';
-                conj.clause[3] = conj.clause[3] + ' ' + to_string(-i) + ' ' + to_string(-(i+spalten)) + ' ' + to_string(-(i-spalten)) + '\n';
-            }
+            conj.clause[0] = conj.clause[0] + ' ' + to_string(-i) + ' ' + to_string(-(i+1)) + ' ' + to_string(-(i+2)) + " 0" + '\n';
+            conj.clause[1] = conj.clause[1] + ' ' + to_string(-i) + ' ' + to_string(-(i+spalten)) + ' ' + to_string(-(i+spalten*2)) + " 0" + '\n';
+
         }
         else
         {
-            conj.clause[0] = conj.clause[0] + ' ' + to_string(-i) + ' ' + to_string(-(i+1)) + ' ' + to_string(-(i+2)) + '\n';
-            conj.clause[2] = conj.clause[2] + ' ' + to_string(-i) + ' ' + to_string(-(i+spalten)) + ' ' + to_string(-(i+spalten)) + '\n';
+            // erste Spalte, außer letztes Feld in erster Spalte
+            if (((i%spalten) == 1) && i<=(zeichen - (spalten*2)))
+            {
+                conj.clause[0] = conj.clause[0] + ' ' + to_string(-i) + ' ' + to_string(-(i+1)) + ' ' + to_string(-(i+2)) + " 0" + '\n';
+                conj.clause[1] = conj.clause[1] + ' ' + to_string(-i) + ' ' + to_string(-(i+spalten)) + ' ' + to_string(-(i+spalten*2)) + " 0" + '\n';
+            }
+            else
+            {
+                // erste Zeile und eine der letzten 2 Spalten
+                if (i<=spalten && ((i%spalten)==0 || (i%spalten==(spalten-1))))
+                {
+                    conj.clause[1] = conj.clause[1] + ' ' + to_string(-i) + ' ' + to_string(-(i+spalten)) + ' ' + to_string(-(i+spalten*2)) + " 0" + '\n';
+                }
+                else
+                {
+                    // Rest erste Spalte
+                    if (i<=spalten)
+                    {
+                    conj.clause[0] = conj.clause[0] + ' ' + to_string(-i) + ' ' + to_string(-(i+1)) + ' ' + to_string(-(i+2)) + " 0" + '\n';
+                    conj.clause[1] = conj.clause[1] + ' ' + to_string(-i) + ' ' + to_string(-(i+spalten)) + ' ' + to_string(-(i+spalten*2)) + " 0" + '\n';
+                    }
+                    else
+                    {
+                        // Feld das in letzten 2 Spalten und letzen 2 Zeilen liegt
+                        if(((i%spalten)==0 || (i%spalten==(spalten-1))) && i>(zeichen-(spalten*2)))
+                        {
+
+                        }
+                        else
+                        {
+                            // Feld das im Rest der letzten 2 Zeilen liegt.
+                            if(i>(zeichen-(spalten*2)))
+                            {
+                                conj.clause[0] = conj.clause[0] + ' ' + to_string(-i) + ' ' + to_string(-(i+1)) + ' ' + to_string(-(i+2)) + " 0" + '\n';
+                            }
+                            else
+                            {
+                                // Feld das im Rest der letzten 2 Spalten liegt.
+                                if((i%spalten)==0 || (i%spalten)==(spalten-1))
+                                {
+                                    conj.clause[1] = conj.clause[1] + ' ' + to_string(-i) + ' ' + to_string(-(i+spalten)) + ' ' + to_string(-(i+spalten*2)) + " 0" + '\n';
+                                }
+                                else
+                                {
+                                    // Restliche Felder
+                                    conj.clause[0] = conj.clause[0] + ' ' + to_string(-i) + ' ' + to_string(-(i+1)) + ' ' + to_string(-(i+2)) + " 0" + '\n';
+                                    conj.clause[1] = conj.clause[1] + ' ' + to_string(-i) + ' ' + to_string(-(i+spalten)) + ' ' + to_string(-(i+spalten*2)) + " 0" + '\n';
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
     return conj;
 }
+
+struct arra same(int zeilen, string var[]) {
+    struct arra conj;
+    conj.clause = new string[2] ;
+     for (int i = 1; i<=zeilen; i++)
+    {
+
+            for (int j = 1; j<= var[i].length(); j++)
+            {
+                switch (var[i][j])
+                {
+                    case '?' :
+                        {
+                            conj.clause[0] = conj.clause[0] + ' '+ to_string(i*j)   ;
+                            conj.clause[1] = conj.clause[1] + ' '+ to_string(-(i*j)) ;
+                            if (j%3 ==0 ) {
+                                conj.clause[0] = conj.clause[0] + '\n' ;
+                                conj.clause[1] = conj.clause[1] + '\n' ;
+                            }
+                        }
+                    case 'B' :
+                        {
+                             conj.clause[0] = conj.clause[0] + ' ' +to_string(i*j) + '\n' ;
+                        }
+                    case 'W' :
+                        {
+                             conj.clause[0] = conj.clause[0] + ' ' +to_string(-(i*j)) + '\n' ;
+                        }
+                }
+
+            }
+    }
+    return conj;
+
+
+}
+
 int zeilenSpalten (std::string a)
 {
     std::stringstream parser;
